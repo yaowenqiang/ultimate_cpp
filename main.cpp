c73379c1cc04a727e656cd0c84160e71cdf9c1c2
姚文强 <yaowenqiang111@163.com>
2023-01-09 00:20:34 +0800

    add variables demo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 int main() {
+    const double pi = 3.14;
+    int file_size = 100; // Snake Case
+    int FileSize = 100; // Pascal Case
+    int fileSize = 100; // Camel Case
+    int iFileSize = 100; // Hungarian Notation, i means variable type
+
+    int counter = 100;
+    double sales = 9.99;
     std::cout << "Hello world!";
+    std::cout << file_size;
+
+    double x = 10;
+    int y = x++; // y = 10, x = 11
+    int y1 = ++x; // y2 = 11, x = 11
+
+    double z = x / y;
+    std::cout << z;
+    std::cout << z;
     return 0;
 }
